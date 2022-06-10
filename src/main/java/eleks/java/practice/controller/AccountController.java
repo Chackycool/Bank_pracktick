a1b4d5ecbb5fb7e25ab1f9f33a6dfc4b568a3aa4
Chackycool <suharskiy7520@gmail.com>
2022-06-10 10:20:38 +0300

    Initial commit

---
+++ src/main/java/eleks/java/practice/controller/AccountController.java
@@ -0,0 +1,30 @@
+package eleks.java.practice.controller;
+import eleks.java.practice.dto.AccountRequestDto;
+import eleks.java.practice.dto.AccountResponseDto;
+import eleks.java.practice.service.AccountServiceImpl;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+@Controller
+public class AccountController {
+    private final AccountServiceImpl accountServiceImpl = new AccountServiceImpl();
+
+    public void create(AccountRequestDto accountRequestDto)
+    {
+        accountServiceImpl.createAccount(accountRequestDto);
+    }
+
+    public List<AccountResponseDto> getAllAccounts()
+    {
+        return accountServiceImpl.getAllAccounts();
+    }
+
+    public AccountResponseDto getAccountById(int id)
+    {
+        return accountServiceImpl.getById(id);
+    }
+
+    public void deleteAccountById(int id){
+        accountServiceImpl.deleteById(id);
+    }
+}
---
+++ src/main/java/eleks/java/practice/repository/AccountRepositoryImpl.java
@@ -0,0 +1,44 @@
+package eleks.java.practice.repository;
+
+import eleks.java.practice.entity.AccountEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+@Repository
+public class AccountRepositoryImpl implements AccountRepository {
+    public final List<AccountEntity> AccountEntityList = new ArrayList<>();
+
+    @Override
+    public void add(AccountEntity accountEntity) {
+        AccountEntityList.add(accountEntity);
+    }
+    @Override
+    public final List<AccountEntity> getAllAccounts() {
+        return AccountEntityList;
+    }
+
+
+    @Override
+    public int getAccountById(int id) {
+        AccountEntity accountEntity = new AccountEntity();
+        for (var i : getAllAccounts()) {
+            if (i.getId() == id) accountEntity = i;
+        }
+        return 0;
+    }
+
+    @Override
+    public void deleteById(int id) {
+        AccountEntity accountEntity = new AccountEntity();
+        for (var i : getAllAccounts()
+        ) {
+            if (i.getId() == id) accountEntity = i;
+        }
+        AccountEntityList.remove(accountEntity);
+    }
+
+    public void PrintBank() {
+
+    }
+}
